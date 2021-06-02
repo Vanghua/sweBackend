30427066aec76d717982222f99a6221e838afd00
Vanghua <1418406055@qq.com>
2021-06-02 11:21:37 +0800

    规范了接口

--- src/main/java/controller/AccountController.java
+++ src/main/java/controller/AccountController.java
@@ -74,7 +74,7 @@ public class AccountController {
 		}
 	}
 	
-	@PostMapping("api/account/login12")
+	@PostMapping("api/account/login112")
 	public String login(@RequestBody LoginInfo loginInfo) {
 		if(Global.ju.exists("select * "
 				+ "from account "
