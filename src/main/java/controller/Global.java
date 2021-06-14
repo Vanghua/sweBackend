f84983c1deb6304eef074bac9d78d5083dc32af1
popomimo <59046279+popomimo@users.noreply.github.com>
2021-06-14 23:50:18 +0800

    两点间经纬度计算 调用 Global.getDistance()

--- src/main/java/controller/Global.java
+++ src/main/java/controller/Global.java
@@ -45,11 +45,27 @@ public class Global {
 	}
 	
 	// SMS configuration
+	/* demo
+	 *  目前这个接口只能用于收到快递的提示，具体请询问 【朱胤璘】
+	 */
 	public static String Uid = "popo1234";
 	public static String SMSkey = "d41d8cd98f00b204e980";
 	public static SMSUtil su = new SMSUtil(Uid, SMSkey);
 	
-	/* demo
-	 *  目前这个接口只能用于收到快递的提示，具体请询问 【朱胤璘】
-	 */
+	// Lng Lat -> Distance
+	public static final double EARTH_RADIUS = 6371393; // 平均半径,单位m 
+	
+    public static double getDistance(Double lat1,Double lng1,Double lat2,Double lng2) {
+        // 经纬度（角度）转弧度。弧度用作参数，以调用Math.cos和Math.sin
+        double radiansAX = Math.toRadians(lng1); // A经弧度
+        double radiansAY = Math.toRadians(lat1); // A纬弧度
+        double radiansBX = Math.toRadians(lng2); // B经弧度
+        double radiansBY = Math.toRadians(lat2); // B纬弧度
+
+        double cos = Math.cos(radiansAY) * Math.cos(radiansBY) * Math.cos(radiansAX - radiansBX)
+            + Math.sin(radiansAY) * Math.sin(radiansBY);
+        double acos = Math.acos(cos); // 反余弦值
+
+        return EARTH_RADIUS * acos; // 最终结果
+    }
 }
